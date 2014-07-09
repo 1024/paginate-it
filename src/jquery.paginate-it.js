fbc8a8646722b9f11b0820a26f3afe674a965cb8
Konrad Podgórski <konrad.podgorski@gmail.com>
2014-07-09 20:12:30 +0200

    expanded version including methods: init,clear,replace,load

--- src/jquery.paginate-it.js
+++ src/jquery.paginate-it.js
@@ -1,66 +1,132 @@
 (function ($) {
     "use strict";
 
-    $.fn.paginateIt = function (options, callback) {
+    var methods = {
+        init: function (element, options, callback) {
+            element.on("click", options.next, function (event) {
+                event.preventDefault();
+                methods.load.call(this, element, options, callback);
+            });
+
+        },
+        clear: function (element, options, callback) {
+            options.page = $.fn.paginateIt.defaults.page -1;
+
+            // clear content of container
+            $(options.container, element).html('');
+
+            // check if callback is a function
+            if (typeof callback === "function") {
+                callback.call(this, element);
+            }
+        },
+        replace: function(element, options, callback) {
+            var container = $(options.container, element);
+            // reset page number
+            options.page = $.fn.paginateIt.defaults.page;
+
+            $.ajax({
+                url: options.baseUrl + "page/" + options.page + "/",
+                dataType: "html"
+            }).done(function (responseText) {
+
+                    var fetchedContent = options.selector ?
+                        $("<div>").append($.parseHTML(responseText)).find(options.selector) :
+                        responseText;
+
+                    // replace mode is hardcoded in replace plugin method
+                    container.html(fetchedContent);
+
+                    // check if callback is a function
+                    if (typeof callback === "function") {
+                        callback.call(this, element);
+                    }
+                });
+
+            // save changed page
+            element.data('paginate-it', options);
+        },
+        load: function (element, options, callback) {
+            // increment page number on each load call
+            options.page++;
+            $.ajax({
+                url: options.baseUrl + "page/" + options.page + "/",
+                dataType: "html"
+            }).done(function (responseText) {
+
+                    var container = $(options.container, element),
+                        fetchedContent = options.selector ?
+                            $("<div>").append($.parseHTML(responseText)).find(options.selector) :
+                            responseText;
+
+                    if (options.mode === "append") {
+                        container.append(fetchedContent);
+                    } else if (options.mode === "replace") {
+                        container.html(fetchedContent);
+                    } else {
+                        // default is replace
+                        container.html(fetchedContent);
+                    }
+
+                    // check if callback is a function
+                    if (typeof callback === "function") {
+                        callback.call(this, element);
+                    }
+                });
+
+            // save changed page
+            element.data('paginate-it', options);
+        }
+    };
+
+    $.fn.paginateIt = function (method, options, callback) {
+
+        if (typeof(method) === "object") {
+            callback = options;
+            options = method;
+        }
 
         if (typeof(options) === "function") {
             callback = options;
             options = {};
         }
 
-        var globalSettings = $.extend({}, $.fn.paginateIt.defaults, options);
+        if (typeof(options) === "undefined") {
+            options = {};
+        }
 
         return this.each(function () {
 
-            var elementSettings = $.extend({}, globalSettings, $(this).data()),
-                self;
+            var self = $(this),
+                elementOptions = self.data('paginate-it');
+            if (!elementOptions) {
+                elementOptions = $.fn.paginateIt.defaults;
+            }
 
-            self = $(this);
+            elementOptions = $.extend({}, elementOptions, options);
 
-            self.on("click", elementSettings.next, function (event) {
-                event.preventDefault();
-                load(self, elementSettings, callback);
-            });
+            self.data('paginate-it', elementOptions);
+
+            if (methods[method]) {
+                return methods[ method ].call(self, self, elementOptions, callback);
+            } else if (typeof method === "object" || !method) {
+                // Default to "init"
+                return methods.init.call(self, self, elementOptions, callback);
+            } else {
+                $.error("Method " + method + " does not exist on jQuery.paginateIt");
+            }
 
             return this;
         });
     };
 
-    // private function that executes ajax loading of next page
-    function load(self, elementSettings, callback) {
-        elementSettings.page++;
-        $.ajax({
-            url: elementSettings.baseUrl + "page/" + elementSettings.page + "/",
-            dataType: "html"
-        }).done(function (responseText) {
-
-                var container = $(elementSettings.container, self),
-                    fetchedContent = elementSettings.selector ?
-                        $("<div>").append($.parseHTML(responseText)).find(elementSettings.selector) :
-                        responseText;
-
-                if (elementSettings.mode === "append") {
-                    container.append(fetchedContent);
-                } else if (elementSettings.mode === "replace") {
-                    container.html(fetchedContent);
-                } else {
-                    // default is replace
-                    container.html(fetchedContent);
-                }
-
-                // check if callback is a function
-                if (typeof callback === "function") {
-                    callback.call(this, self);
-                }
-            });
-    }
-
     $.fn.paginateIt.defaults = {
-        baseUrl: null,
-        mode: "append",
-        container: null,
-        selector: null,
-        next: null
+        page: 1, // current page, each load request increment it by 1, clear set it to 0
+        baseUrl: null, // on every load request to that baseUrl plugin is appending: "page/" + page, warning: it must end with trailing slash
+        mode: "append", // available modes are append (default) and replace
+        selector: null, // used to extract items from fetched page
+        container: null, // where to put fetched items
+        next: null // css selector to listen for
     };
 
 })(jQuery);
